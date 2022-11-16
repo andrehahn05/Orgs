2b47987b0572bde87ddc895592d43124c21cae49
andrehahn05 <andrehahn05@gmail.com>
2022-11-16 17:52:09 -0400

    activity_form_product

---
+++ app/src/main/java/com/hahn/orgs/FormProductActivity.kt
@@ -0,0 +1,11 @@
+package com.hahn.orgs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_product)
+    }
+}

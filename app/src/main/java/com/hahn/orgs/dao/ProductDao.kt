8e5b83a03f643a6166ba3c49158c8160b1d70781
andrehahn05 <andrehahn05@gmail.com>
2022-11-22 11:46:01 -0400

    drawable-xdpi/product

--- app/src/main/java/com/hahn/orgs/dao/ProductDao.kt
+++ app/src/main/java/com/hahn/orgs/dao/ProductDao.kt
@@ -1,18 +1,29 @@
 package com.hahn.orgs.dao
 
 import com.hahn.orgs.model.Product
+import java.math.BigDecimal
 
-class ProductDao {
+class ProductDao
+{
 
-    fun store(product:Product) {
+    fun store(product: Product)
+    {
         products.add(product)
     }
 
-    fun findAll():List<Product> {
+    fun findAll(): List<Product>
+    {
         return products.toList()
     }
 
-    companion object {
-        private val products = mutableListOf<Product>()
+    companion object
+    {
+        private val products = mutableListOf<Product>(
+            Product(
+                name = "Frutas",
+                description = "um pouco de tudo",
+                price = BigDecimal("39.49")
+            )
+        )
     }
 }
